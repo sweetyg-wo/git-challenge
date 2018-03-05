2daf0e6c6404162ab65e2e09c2175cffed5cadfe
David Guttman <david@davidguttman.com>
2018-03-05 08:56:36 -0800

    feat: add echo route
    feat: add reverse route

--- test/routes.js
+++ test/routes.js
@@ -91,11 +91,16 @@ tape('should get reverse', function (t) {
   }
 
   var url = '/reverse/' + expected.input
+  t.deepEqual(res.body, expected, 'values should match')
+  t.end()
+})
+
+tape('should get echo', function (t) {
+  var url = '/echo?one=1&two=2'
   servertest(server(), url, {encoding: 'json'}, function (err, res) {
     t.ifError(err, 'no error')
-
     t.equal(res.statusCode, 200, 'correct statusCode')
-    t.deepEqual(res.body, expected, 'values should match')
+    t.deepEqual(res.body, {one: '1', two: '2'}, 'values should match')
     t.end()
   })
 })
